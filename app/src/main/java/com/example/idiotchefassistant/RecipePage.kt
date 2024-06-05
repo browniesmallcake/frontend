5cdcc16b9c6259c0992d7a744987be6df25a23c4
browniesmallcake <ironingot0172@gmail.com>
2024-06-06 03:15:51 +0800

    test

--- app/src/main/java/com/example/idiotchefassistant/RecipePage.kt
+++ app/src/main/java/com/example/idiotchefassistant/RecipePage.kt
@@ -1,9 +1,6 @@
 package com.example.idiotchefassistant
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.idiotchefassistant.databinding.ActivityRecipePageBinding
@@ -16,30 +13,34 @@ class RecipePage : AppCompatActivity() {
         binding = ActivityRecipePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val items = generateFakeData(10)
+/*        val items = generateFakeData(10)
         val commentSection = binding.commentSection
 
-        items.forEach {it ->
+        items.forEach {
             val commentView = LayoutInflater.from(this).inflate(R.layout.recipe_commit_item, commentSection, false)
-            val userImage = commentView.findViewById<ImageView>(R.id.user_image)
-            val userName = commentView.findViewById<TextView>(R.id.user_name)
-            val userReview = commentView.findViewById<TextView>(R.id.user_review)
-            val userComment = commentView.findViewById<TextView>(R.id.user_comment)
+            if (commentView != null) {
+                val userImage = commentView.findViewById<ImageView>(R.id.user_image)
+                val userName = commentView.findViewById<TextView>(R.id.user_name)
+                val userComment = commentView.findViewById<TextView>(R.id.user_comment)
+                val userReview = commentView.findViewById<TextView>(R.id.user_review)
 
-            userImage.setImageResource(it.userImg)
-            userName.text = it.userName
-            userComment.text = it.commit
-            userReview.text = it.review.toString()
+                userImage.setImageResource(it.userImg)
+                userName.text = it.userName
+                userComment.text = it.commit
+                userReview.text = it.review.toString()
 
-            commentSection.addView(commentView)
-        }
+                commentSection.addView(commentView)
+            } else {
+                Log.e("RecipePage", "commentView is null")
+            }
+        }*/
     }
-    private fun generateFakeData(cnt: Int): List<CommitItem> {
+/*    private fun generateFakeData(cnt: Int): List<CommitItem> {
         val fakeData = mutableListOf<CommitItem>()
         for (i in 1..cnt) {
             fakeData.add(CommitItem(R.drawable.logo, "用戶 $i", "這是一個評論", 5))
         }
         return fakeData
-    }
+    }*/
 }
 
