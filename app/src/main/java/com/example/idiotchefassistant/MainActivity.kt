73c3cfe8bff04c528556acd1284582e5c242dc32
browniesmallcake <ironingot0172@gmail.com>
2024-09-22 22:39:33 +0800

    移除Warning

--- app/src/main/java/com/example/idiotchefassistant/MainActivity.kt
+++ app/src/main/java/com/example/idiotchefassistant/MainActivity.kt
@@ -2,41 +2,26 @@ package com.example.idiotchefassistant
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.lifecycle.ViewModelProviders
 import com.example.idiotchefassistant.recipeBlock.HomePage
 import com.example.idiotchefassistant.recipeBlock.SearchPage
 import com.example.idiotchefassistant.databinding.ActivityMainBinding
-import com.example.idiotchefassistant.itemBlock.IngredientFactory
-import com.example.idiotchefassistant.itemBlock.IngredientItem
-import com.example.idiotchefassistant.itemBlock.IngredientRepository
-import com.example.idiotchefassistant.itemBlock.IngredientViewModel
-import com.example.idiotchefassistant.resultBlock.ingredientService
 import com.example.idiotchefassistant.ui.login.LoginActivity
 import com.google.android.material.navigation.NavigationView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var ingredientViewModel: IngredientViewModel
-    private lateinit var ingredientFactory: IngredientFactory
-    private lateinit var ingredientRepository: IngredientRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        ingredientRepository = IngredientRepository()
-        ingredientFactory = IngredientFactory(ingredientRepository)
-        ingredientViewModel = ViewModelProviders.of(this, ingredientFactory).get(IngredientViewModel::class.java)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -58,6 +43,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 .replace(R.id.fragment_container, HomePage()).commit()
             navigationView.setCheckedItem(R.id.nav_home)
         }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    // 可以直接調用此方法來關閉當前 Activity 或 Fragment
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+
     }
 
 
@@ -98,14 +96,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
-    }
-
-    override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
     }
 
 }
